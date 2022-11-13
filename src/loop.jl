d8317e4e4ca6a98f3d07b9a3631fcc7df5a51dd9
Arnab Bachhar <86238928+arnab82@users.noreply.github.com>
2022-11-13 17:01:53 -0500

    Update loop.jl

--- src/loop.jl
+++ src/loop.jl
@@ -223,3 +223,29 @@ while true
     if (m,n) == (6,6) break end
 end
 println("It took $numtries tries to get (6,6).")
+arr = [5, 8, 12, 17, 24, 42];
+
+[x^2 for x in arr]
+
+[(x^2, y^3) for x ∈ 1:4, y ∈ 1:3]
+[x^2 for x in arr if iseven(x)]
+
+
+[iseven(x) ? x^2 : x^3 for x in arr]
+
+
+[(i,j) for i=1:4 for j=1:i]
+
+
+(x^2 for x ∈ 1:1000)
+
+sum(x^2 for x ∈ 1:1000)
+
+s = 0
+@time for i = 1:1_000_000_000 s += i^2 end
+
+@time sum([i^2 for i = 1:1_000_000_000])
+
+@time sum(i^2 for i ∈ 1:1_000_000_000)
+
+
